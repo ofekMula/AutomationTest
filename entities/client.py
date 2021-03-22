e647e1912b9568d97200fa1af03d2e7356c06072
Ofek Mula <ofek1210@gmail.com>
2021-03-23 00:15:43 +0200

    chat is working

---
+++ entities/client.py
@@ -0,0 +1,15 @@
+class Client:
+    def __init__(self, session_id, nickname):
+        self._session_id = session_id
+        self._nickname = nickname
+
+    def get_client_sid(self):
+        return self._session_id
+
+    def get_client_nickname(self):
+        return self._nickname
+
+    def set_nickname(self, nickname):
+        self._nickname = nickname
+
+
